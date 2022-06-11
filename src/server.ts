7f99f94cab21662ad13bac9337799f344e83e0c8
jfdsouza988 <juliosouza.rs@gmail.com>
2022-06-11 16:21:07 -0300

    criado container do app e database com docker

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,4 @@ import express from 'express';
 
 const app = express();
 
-app.listen(3000, () => console.log('Server is running on PORT 3000'));
+app.listen(3333, () => console.log('Server is running on PORT 3333'));

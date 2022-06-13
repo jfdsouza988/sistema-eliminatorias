0ace81f25d09395ea3ce38d4dfd7758f864b7583
jfdsouza988 <juliosouza.rs@gmail.com>
2022-06-13 18:00:15 -0300

    testes unitarios para rota de inicio do campeonato

---
+++ src/modules/championships/useCases/startChampionship/StartChampionship.spec.ts
@@ -0,0 +1,72 @@
+import { ChampionshipStatus } from '@prisma/client';
+import { MatchRepositoryInMemory } from '../../../matchs/repositories/in-memory/MatchRepositoryInMemory';
+import { ChampionshipRepositoryInMemory } from '../../repositories/in-memory/ChampionshipRepositoryInMemory';
+import { CreateChampionshipUseCase } from '../createChampionship/CreateChampionshipUseCase';
+import { StartChampionshipUseCase } from './StartChampionshipUseCase';
+import { RegisterTeamsInChampionshipUseCase } from '../registerTeamsInChampionship/RegisterTeamsInChampionshipUseCase';
+import { AppError } from '../../../../shared/errors/AppError';
+
+let createChampionshipUseCase: CreateChampionshipUseCase;
+let registerTeamsInChampionshipUseCase: RegisterTeamsInChampionshipUseCase;
+let championshipRepositoryInMemory: ChampionshipRepositoryInMemory;
+let matchRepositoryInMemory: MatchRepositoryInMemory;
+let startChampionshipUseCase: StartChampionshipUseCase;
+
+describe('Register teams in championship', () => {
+  beforeEach(() => {
+    championshipRepositoryInMemory = new ChampionshipRepositoryInMemory();
+    matchRepositoryInMemory = new MatchRepositoryInMemory();
+    createChampionshipUseCase = new CreateChampionshipUseCase(championshipRepositoryInMemory);
+    startChampionshipUseCase = new StartChampionshipUseCase(championshipRepositoryInMemory, matchRepositoryInMemory);
+    registerTeamsInChampionshipUseCase = new RegisterTeamsInChampionshipUseCase(championshipRepositoryInMemory);
+  });
+
+  it('should not be able to start championship that does not exists', async () => {
+    expect(async () => {
+      await startChampionshipUseCase.execute('Championship Test');
+    }).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should not be able to start championship with inappropriate number of teams', async () => {
+    expect(async () => {
+      const championship = {
+        name: 'Championship Test',
+        description: 'Championship description Teste',
+        award: 1000,
+      };
+
+      await createChampionshipUseCase.execute({
+        name: championship.name,
+        description: championship.description,
+        award: championship.award,
+      });
+
+      await startChampionshipUseCase.execute('Championship Test');
+    }).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should be able to start a championship', async () => {
+    const championship = {
+      name: 'Championship Test',
+      description: 'Championship description Teste',
+      award: 1000,
+    };
+
+    const teams = [
+      { name: 'Team1', abbreviation: 'TM1' },
+      { name: 'Team2', abbreviation: 'TM2' },
+    ];
+
+    await createChampionshipUseCase.execute({
+      name: championship.name,
+      description: championship.description,
+      award: championship.award,
+    });
+
+    await registerTeamsInChampionshipUseCase.execute({ championshipName: championship.name, teams });
+
+    const championshipStarted = await startChampionshipUseCase.execute(championship.name);
+
+    expect(championshipStarted.status).toBe(ChampionshipStatus.INITIATED);
+  });
+});
---
+++ src/modules/matchs/repositories/in-memory/MatchRepositoryInMemory.ts
@@ -0,0 +1,56 @@
+import { Match, MatchStatus } from '@prisma/client';
+import { randomUUID } from 'crypto';
+import { ICreateMatchDTO } from 'modules/matchs/dtos/ICreateMatchDTO';
+import { IMatchRepository } from '../IMatchRepository';
+
+class MatchRepositoryInMemory implements IMatchRepository {
+  matchs: Match[] = [];
+
+  async create(data: ICreateMatchDTO): Promise<Match> {
+    const match = {
+      id: randomUUID(),
+      matchNumber: data.matchNumber,
+      teamA: data.teamA,
+      resultTeamA: null,
+      teamB: data.teamB,
+      resultTeamB: null,
+      winner: null,
+      championshipId: data.championshipId,
+      status: MatchStatus.PROGRESS,
+      updatedAt: new Date(),
+      createdAt: new Date(),
+    };
+
+    this.matchs.push(match);
+
+    return match;
+  }
+
+  async updateResult(id: string, teamA: number, teamB: number, winner: string): Promise<Match> {
+    const match = this.matchs.find((match) => match.id === id) as Match;
+
+    match.resultTeamA = teamA;
+    match.resultTeamB = teamB;
+    match.winner = winner;
+
+    return match;
+  }
+
+  async findById(id: string): Promise<Match | null> {
+    return this.matchs.find((match) => match.id === id) as Match;
+  }
+
+  async findMatchsInProgressByChampionship(championshipId: string): Promise<Match[]> {
+    let matchsInProgress: Match[] = [];
+
+    this.matchs.forEach((match) => {
+      if (match.championshipId === championshipId && match.status === 'PROGRESS') {
+        matchsInProgress.push(match);
+      }
+    });
+
+    return matchsInProgress;
+  }
+}
+
+export { MatchRepositoryInMemory };
